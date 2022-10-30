b878e8a495ad64f3d79cfda073508ab33edbd46d
k18gurukul <117011821+k18gurukul@users.noreply.github.com>
2022-10-30 22:09:17 +0530

    Add files via upload

--- app-flutter/Muhammad Ali/food-panda/users-app/lib/mainScreens/home_screen.dart
+++ app-flutter/Muhammad Ali/food-panda/users-app/lib/mainScreens/home_screen.dart
@@ -1,6 +1,13 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:foodpanda_users_app/authentication/auth_screen.dart';
 import 'package:foodpanda_users_app/global/global.dart';
+import 'package:foodpanda_users_app/models/sellers.dart';
+import 'package:foodpanda_users_app/widgets/info_design.dart';
+import 'package:foodpanda_users_app/widgets/my_drawer.dart';
+import 'package:foodpanda_users_app/widgets/progress_bar.dart';
 
 
 
@@ -13,7 +20,39 @@ class HomeScreen extends StatefulWidget {
 
 
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+{
+  final items = [
+    "slider/0.jpg",
+    "slider/1.jpg",
+    "slider/2.jpg",
+    "slider/3.jpg",
+    "slider/4.jpg",
+    "slider/5.jpg",
+    "slider/6.jpg",
+    "slider/7.jpg",
+    "slider/8.jpg",
+    "slider/9.jpg",
+    "slider/10.jpg",
+    "slider/11.jpg",
+    "slider/12.jpg",
+    "slider/13.jpg",
+    "slider/14.jpg",
+    "slider/15.jpg",
+    "slider/16.jpg",
+    "slider/17.jpg",
+    "slider/18.jpg",
+    "slider/19.jpg",
+    "slider/20.jpg",
+    "slider/21.jpg",
+    "slider/22.jpg",
+    "slider/23.jpg",
+    "slider/24.jpg",
+    "slider/25.jpg",
+    "slider/26.jpg",
+    "slider/27.jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,21 +75,80 @@ class _HomeScreenState extends State<HomeScreen> {
           sharedPreferences!.getString("name")!,
         ),
         centerTitle: true,
-        automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Logout"),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.cyan,
+      drawer: MyDrawer(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * .3,
+                width: MediaQuery.of(context).size.width,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: MediaQuery.of(context).size.height * .3,
+                    aspectRatio: 16/9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 2),
+                    autoPlayAnimationDuration: const Duration(milliseconds: 500),
+                    autoPlayCurve: Curves.decelerate,
+                    enlargeCenterPage: true,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                  items: items.map((index) {
+                    return Builder(builder: (BuildContext context){
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 1.0),
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Image.asset(
+                              index,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      );
+                    });
+                  }).toList(),
+                ),
+              ),
+            ),
           ),
-          onPressed: ()
-          {
-            firebaseAuth.signOut().then((value){
-              Navigator.push(context, MaterialPageRoute(builder: (c)=> const AuthScreen()));
-            });
-          },
-        ),
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection("sellers")
+                .snapshots(),
+            builder: (context, snapshot)
+            {
+              return !snapshot.hasData
+                  ? SliverToBoxAdapter(child: Center(child: circularProgress(),),)
+                  : SliverStaggeredGrid.countBuilder(
+                      crossAxisCount: 1,
+                      staggeredTileBuilder: (c) => StaggeredTile.fit(1),
+                      itemBuilder: (context, index)
+                      {
+                        Sellers sModel = Sellers.fromJson(
+                          snapshot.data!.docs[index].data()! as Map<String, dynamic>
+                        );
+                        //design for display sellers-cafes-restuarents
+                        return InfoDesignWidget(
+                          model: sModel,
+                          context: context,
+                        );
+                      },
+                      itemCount: snapshot.data!.docs.length,
+                    );
+            },
+          ),
+        ],
       ),
     );
   }
